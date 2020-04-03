cf3b293f66723b76e6243c860a81e78856276434
Marcelo Cueto <cueto@live.cl>
2020-04-03 15:43:32 -0300

    Add Find function based on https://golangcode.com/check-if-element-exists-in-slice/

---
+++ kissfind.go
@@ -0,0 +1,19 @@
+package kissfind
+
+import (
+	"os"
+	"fmt"
+	"log"
+	"github.com/joho/godotenv"
+)
+
+// Find takes a slice and looks for an element in it. If found it will
+// return it's key, otherwise it will return -1 and a bool of false.
+func Find(slice []string, val string) (int, bool) {
+    for i, item := range slice {
+        if item == val {
+            return i, true
+        }
+    }
+    return -1, false
+}
